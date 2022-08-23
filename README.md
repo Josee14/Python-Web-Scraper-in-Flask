8cd9a8a4c9f4e18eba9ce15610c698734c29f272
Joseph Ndungi <josephn@innova.co.ke>
2022-08-23 12:06:52 +0300

    code doc

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Python-Web-Scraper-in-Flask
+
+ ### <u> What is a web scraper? </u>
+ Websites currently contain a wealth of valuable information.When gathering this information, you will almost definitely find yourself manually copying and pasting.You need a simple and more automated method for this, which is where web scraping comes in.
+
+ Web scraping is imply the automated extraction of a web page's unstructured HTML information in a specified format, structuring it, and storing it in a database or saving it as a CSV file for future consumption.
+
+### How to scrape the web?
+1. Find the URLs you want to scrape
+2. Inspect the page
+3. Identify the data you want to extract- find appropriate nest tags
+
+### What tools can you use to scrape the web?
+1. Beautiful Soup
+2. Scrapy
+3. Pandas
+4. Parsehub
+
+## In this tutorial, we'll look at web scraping using Beautiful Soup and Requests. We'll build a web scrapper app with Flask, Python's most lightweight web framework.
+How it works
+1. Load the application
+2. Provide a target URL and a tag to be fetched example img,p, title
+3. Receive a response - the requested element(s) content.
+4. For images, there will be a download functionality that will save the images to your downloads directory
+
+### Project details.
+Our primary discussion files will be as follows:
+
+##### app.py
+##### templates/index.html
+### app.py
+We will utilize Flask, Beautiful Soup, and request libraries. First and foremost, we'll import some functionality from Flask and Beautiful Soup into the app.py file.
+
+### We need to validate and parse the URLs we get, so we import the URL handling module for Python- urllib, as well as some other built-in libraries.
+
+    from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    flash,
+    url_for
+    )
+    import urllib.request 
+    from urllib.parse import urlparse,urljoin
+    from bs4 import BeautifulSoup
+    import requests,validators,uuid,pathlib,os
+
+### Next, for our first route, we'll construct a function that returns an HTML template, as seen below.
+     @app.route("/",methods=("GET", "POST"), strict_slashes=False)
+     def index():
+     # parsing
+
+     return render_template("index.html")
+
+### To begin our scrap, we must ensure that the user has sent a Post request.
+     if request.method == "POST":
+
+### Parsing
+check code line 39-...
+
+### Downloading images
+
+    
--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import (
     request,
     redirect,
     flash,
-    url_for,
-    current_app
+    url_for
 )
 import urllib.request 
 from urllib.parse import urlparse,urljoin
@@ -15,7 +14,9 @@ import requests,validators,uuid,pathlib,os
 app = Flask(__name__)
 app.secret_key = "secret-key"
 
-
+'''The function checks if the provided tag is an image tag, then extracts the
+images' src attribute value and verifies it to see if it's an absolute path.
+If else, it joins the relative path to the target's base URL.'''
 def image_handler(tag,specific_element,requested_url):
     image_paths = []
 
@@ -38,15 +39,25 @@ def index():
     if request.method == "POST":
 
         try:
+            '''we are writing a function to accept image paths and generate a complete URL from them, 
+            the form field inputs must be available globally, so we define them as global variables.'''
             global requested_url,specific_element,tag
-
+           
+            #set values to the two global variables from our form fields
             requested_url = request.form.get('urltext')
             tag = request.form.get('specificElement')
 
+            '''We send HTTP request to the user-specified URL - requested_url. The server answers
+             the request by delivering the raw HTML content of the webpage, which we then 
+             transform to text- .text() and assign to the variable source.'''
             source = requests.get(requested_url).text
-            # parser library?
+
+            '''Parse the page after we've extracted the HTML content in text format, and we'll 
+            utilize html.parser as our parsing library'''
             soup = BeautifulSoup(source, "html.parser")
-            
+
+            '''we navigate through the tree to discover the element we require. The tag that
+             the user will enter in the form field'''
             specific_element = soup.find_all(tag)
             
             counter = len(specific_element)
@@ -56,7 +67,9 @@ def index():
                 specific_element,
                 requested_url
                 )
-
+            
+            '''pass the results of our parsing along with the return statement to make them
+             available on the HTML template.'''
             return render_template("index.html",
                 url = requested_url,
                 counter=counter,
@@ -75,8 +88,10 @@ def downloader():
     try:
         for img in image_handler(tag,specific_element,requested_url):
             image_url = img
-
+            
+            #The uuid library is used by the download function above to produce unique names for the downloaded files.
             filename = str(uuid.uuid4())
+            #strip the image extension from the image path
             file_ext = pathlib.Path(image_url).suffix
 
             picture_filename = filename + file_ext
@@ -84,7 +99,8 @@ def downloader():
             downloads_path = str(pathlib.Path.home() / "Downloads")
 
             picture_path  = os.path.join(downloads_path, picture_filename)
-
+            
+            #actual image download
             urllib.request.urlretrieve(image_url, picture_path)
 
 
